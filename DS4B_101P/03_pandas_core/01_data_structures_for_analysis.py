30d588454c4b7fe564c3a103508bfe1bb64c5201
Maxim <demuramax@gmail.com>
2022-05-11 00:17:30 +0300

    pandas added

---
+++ DS4B_101P/03_pandas_core/01_data_structures_for_analysis.py
@@ -0,0 +1,66 @@
+# DS4B 101-P: PYTHON FOR DATA SCIENCE AUTOMATION ----
+# Module 3 (Pandas Core): Data Structures ----
+
+# IMPORTS ----
+import pandas as pd
+import numpy as np
+
+from my_pandas_extensions.database import collect_data
+
+
+# 1.0 HOW PYTHON WORKS - OBJECTS
+
+# Objects
+
+
+# Objects have classes
+
+
+# Objects have attributes
+
+
+
+# Objects have methods
+
+
+
+
+# 2.0 KEY DATA STRUCTURES
+
+# - PANDAS DATA FRAME
+
+
+
+# - PANDAS SERIES
+
+
+
+
+# - NUMPY
+
+
+
+
+
+
+# 3.0 DATA STRUCTURES - PYTHON
+
+# Dictionaries
+
+
+
+# Lists
+
+
+
+# Tuples
+
+
+# Base Data Types
+
+
+
+
+# Casting
+
+
