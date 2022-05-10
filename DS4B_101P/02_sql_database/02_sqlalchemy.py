f40896bcb65f1c73f8991b74a17692524deeeccf
Maxim <demuramax@gmail.com>
2022-05-11 00:03:38 +0300

    sql database func added

--- DS4B_101P/02_sql_database/02_sqlalchemy.py
+++ DS4B_101P/02_sql_database/02_sqlalchemy.py
@@ -2,29 +2,60 @@
 # SQL DATABASES (Module 2): Working with SQLAlchemy ----
 
 # IMPORTS ----
+# %%
+import pandas as pd
+import sqlalchemy as sql
+import os
 
+os.mkdir('00_database')
 # CREATING A DATABASE ----
 
 # Instatiate a database
 
-
+engine = sql.create_engine('sqlite:///00_database/bike_orders_database.sqlite')
+conn = engine.connect()
 # Read Excel Files
-
+# %%
+bikes_df = pd.read_excel('../01_jumpstart/bikes.xlsx')
+bikeshops_df = pd.read_excel('../01_jumpstart/bikeshops.xlsx')
+orderlines_df = pd.read_excel('../01_jumpstart/orderlines.xlsx')
 
 # Create Tables
+bikes_df.to_sql('bikes', con=conn)
 
+pd.read_sql('select * from bikes', con=conn)
+
+bikeshops_df.to_sql('bikeshops', con=conn)
+pd.read_sql('select * from bikeshops', con=conn)
+
+orderlines_df.iloc[: , 1:].to_sql('orderlines', con=conn, if_exists='replace')
+pd.read_sql('select * from orderlines', con=conn)
 
 # Close Connection
+# %%
+conn.close()
 
 # RECONNECTING TO THE DATABASE 
+# %%
 
 # Connecting is the same as creating
+engine = sql.create_engine('sqlite:///00_database/bike_orders_database.sqlite')
 
-
+conn = engine.connect()
 # GETTING DATA FROM THE DATABASE
-
+# %%
 # Get the table names
+engine.table_names()
 
+inspector = sql.inspect(conn)
+inspector.get_schema_names()
 
+inspector.get_table_names()
 # Read the data
+# %%
+table = inspector.get_table_names()
+pd.read_sql(f'select * from {table[2]}', con=conn)
 
+
+conn.close()
+# %%
--- DS4B_101P/02_sql_database/03_func_collect_data.py
+++ DS4B_101P/02_sql_database/03_func_collect_data.py
@@ -2,6 +2,88 @@
 # SQL DATABASES (Module 2): Working with SQLAlchemy ----
 
 # IMPORTS ----
-
+import sqlalchemy as sql
+import pandas as pd
 # FUNCTION DEFINITION ----
+# %%
 
+def collect_data(conn_string = 'sqlite:///00_database/bike_orders_database.sqlite'):
+    """
+    Collects and combines the bike orders data.
+
+    Args:
+        conn_string (str, optional): A SQLAlchemy connectio string to find the database. 
+        Defaults to 'sqlite:///00_database/bike_orders_database.sqlite'.
+
+    Returns:
+        DataFrame: A pandas data frame that combines data from tables:
+        - orderlines: Transactions data
+        - bikes: Products data
+        - bikeshops: customers data
+    """
+    # 1.0 Connect to database 
+    engine = sql.create_engine(conn_string)
+    
+    conn = engine.connect()
+    
+    table_names = ['bikes', 'bikeshops', 'orderlines']
+    
+    data_dict = {}
+    for table in table_names:
+        data_dict[table] = pd.read_sql(f'select * from {table}', con=conn) \
+            .drop('index', axis=1)
+        
+    conn.close()
+    # 2.0 Combining and Cleanin Data
+    
+    joined_df = pd.DataFrame(data_dict['orderlines']).merge(
+        right = data_dict['bikes'],
+        how = 'left',
+        left_on = 'product.id',
+        right_on='bike.id'
+    ).merge(
+        right = data_dict['bikeshops'],
+        how = 'left',
+        left_on = 'customer.id',
+        right_on = 'bikeshop.id'
+    )
+    
+    # 3.0 Cleaning Data
+    df = joined_df
+    df['order.date'] = pd.to_datetime(df['order.date'])
+    
+    temp_df = df['description'].str.split(' - ', expand = True)
+    df['category.1'] = temp_df[0]
+    df['category.2'] = temp_df[1]
+    df['frame.material'] = temp_df[2]
+    
+    temp_df = df['location'].str.split(', ', n=1, expand = True)
+    df['city'] = temp_df[0]
+    df['state'] = temp_df[1]
+    
+    df['total.price'] = df['quantity'] * df['price']
+    
+    cols_to_keep_list = [
+    'order.id', 
+    'order.line', 
+    'order.date',  
+    'model', 
+    'quantity',
+    'price', 
+    'total.price',
+    'bikeshop.name', 
+    'category.1', 
+    'category.2',
+    'frame.material', 
+    'city', 
+    'state' 
+    ]
+    df = df[cols_to_keep_list]
+    
+    df.columns = df.columns.str.replace('.', '_')
+    
+    return df
+
+collect_data()
+
+# %%
