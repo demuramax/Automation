3c7b56c08804436ca7a8cf051d82e765d73bdc0a
Maxim <demuramax@gmail.com>
2022-05-10 21:47:24 +0300

    imp files

--- DS4B_101P/02_sql_database/01_import_files.py
+++ DS4B_101P/02_sql_database/01_import_files.py
@@ -2,14 +2,21 @@
 # Module 2 (Pandas Import): Importing Files ----
 
 # IMPORTS ----
-
+# %%
+import pandas as pd
+from os import getcwd, mkdir
 # 1.0 FILES ----
 
 # - Pickle ----
-
-
+# %%
+pickle_df = pd.read_pickle('../01_jumpstart/00_data_wrangled/bike_orderlines_wrangled_df.pkl')
+pickle_df.info()
 # - CSV ----
-
-
+csv_df = pd.read_csv('../01_jumpstart/00_data_wrangled/bike_orderlines_wrangled_df.csv', parse_dates=['order_date'])
+csv_df.info()
 # - Excel ----
+excel_df = pd.read_excel('../01_jumpstart/00_data_wrangled/bike_orderlines_wrangled_df.xlsx')
+excel_df
 
+
+# %%
